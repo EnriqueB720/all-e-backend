c758375c575319a4888f23e3462e44981f704394
EnriqueB720 <enrique21102001@hotmail.com>
2025-08-19 18:05:39 -0600

    fix(Watch module): ownershiplog module needed to be imported

--- src/api/watch/watch.module.ts
+++ src/api/watch/watch.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { WatchResolver } from './watch.resolver';
 
 import { WatchService } from './watch.service';
+import { OwnershipLogModule } from '../ownership-log/ownership-log.module';
+
 
 @Module({
-  imports: [],
+  imports: [OwnershipLogModule],
   providers: [WatchResolver, WatchService],
   exports: [WatchResolver, WatchService],
 })
